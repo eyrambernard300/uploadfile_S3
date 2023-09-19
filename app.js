49c4421278251dd900a3ad8598a981fea79eb702
eyrambernard300 <beyram300@gmail.com>
2023-09-19 12:56:59 +0000

    update with env

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const fs = require('fs')
 
 const s3Client = new S3Client({
     region: 'us-north-1',
-    endpoint: 'https://s3-us-north-1.amazonaws.com/Learn-aws-storage',
+    endpoint: process.env.ENDPOINT,
     credential: {
-        accessKeyId: 'AKIAZZSMIBOGLUMCSBHI',
-        secretAccessKey: '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'
+        accessKeyId: process.env.SECRET_KEY,
+        secretAccessKey:process.env.SECRETACCESSKEY
     },
 })
 
@@ -15,13 +15,13 @@ const uploadFile = async () => {
     try {
         const fileCcontent = fs.readFileSync('mine.png')
         const object = new PutObjectCommand({
-            bucket:'Learn-aws-storage',
+            bucket:process.env.BUCKET,
             key: 'mine.png',
             body: "fileContent"
 
     })
         await S3Client.send(object)
-        // endpoint/bucketname/key
+        // endpoint/bucketname/key  
         console.log('')
     }catch(error) {console.log(error)}
 }
